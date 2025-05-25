27438e82c1306a5529f0af222f4ff68e42a0247e
gaudicos <gaudicoschristian4@gmail.com>
2025-05-25 16:15:46 +0800

    Add STATICFILES_DIRS to serve static files

--- i_track/settings.py
+++ i_track/settings.py
@@ -23,6 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
