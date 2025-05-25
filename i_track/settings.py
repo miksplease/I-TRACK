5471bc299b1ef7a9a9c8c6f6192207c3d07eded9
gaudicos <gaudicoschristian4@gmail.com>
2025-05-25 15:45:04 +0800

    Add STATIC_ROOT setting for static files

--- i_track/settings.py
+++ i_track/settings.py
@@ -21,6 +21,8 @@ logging.basicConfig(level=logging.DEBUG)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
